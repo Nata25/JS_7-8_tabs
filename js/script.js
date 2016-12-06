5c51d8507792c71103636b3947fd68a9039c83e5
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-12-06 11:35:36 +0200

    Try to implement tabs via absolute positioning

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(function() {
-    //
+    console.log("hello");
+    $(".tab__text").hide();
+
+    $(".tab__heading").click(function() {
+        console.log("inslide click");
+        $(this).next(".tab__text").show();
+    })
 });
