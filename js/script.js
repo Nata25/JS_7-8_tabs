fa757c576a39f0d2878adbed94aaadbcf70fa471
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-12-06 13:53:34 +0200

    Finish styling and check css via lint

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 $(function() {
+    
+    $(".tab").not("#tab-h1").hide();
+
     $(".heading").click(function() {
         $("li").not(this).removeClass("heading_active");
-        $(".tab").removeClass("tab_active");
+        $(".tab").hide();
 
         $(this).addClass("heading_active");
         var currentTab = $("#tab-" + $(this).attr("id"));
-        currentTab.addClass("tab_active");
-        var zIndex = currentTab.css("z-index");
-        currentTab.css("z-index", zIndex++);
+        currentTab.fadeIn(300);
     }); // end of listener
 }); // end of ready()
