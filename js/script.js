c00bbd8733c7a8b6ac6192e330537d25c087c052
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-12-06 13:10:28 +0200

    Revise code a bit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 $(function() {
-
     $(".heading").click(function() {
-        $(".heading").removeClass("heading_active");
+        $("li").not(this).removeClass("heading_active");
         $(".tab").removeClass("tab_active");
 
         $(this).addClass("heading_active");
-        var heading = $(this).attr("id");
-        var currentTab = $("#tab-" + heading);
+        var currentTab = $("#tab-" + $(this).attr("id"));
+        currentTab.addClass("tab_active");
         var zIndex = currentTab.css("z-index");
         currentTab.css("z-index", zIndex++);
-        currentTab.addClass("tab_active");
-    })
-});
+    }); // end of listener
+}); // end of ready()
